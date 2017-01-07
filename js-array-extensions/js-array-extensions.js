b5dae13b55da0e682d55d8675ade128423ef89c8
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2017-01-07 09:21:11 +0200

    array extension is done

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,12 @@
-// your code here
+var ArrayExtensions = function(){
+	var arrayMethods ={} ;
+	arrayMethods.first = function(arr){
+		return arr[0] ;
+	}
+
+	arrayMethods.last = function (arr){
+		return arr[arr.length-1]
+	}
+
+	return arrayMethods ;
+}
